67ab2db3833fc3c10d16bb7498bb684f9bfc45c2
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2021-12-09 04:51:29 +0800

    preserve endline

--- lcd_display.py
+++ lcd_display.py
@@ -47,7 +47,8 @@ def show_on_lcd(full_text):
     # Remember that your sentences can only be 16 characters long!
     print("Writing to display")
     display.lcd_clear()
-    lines = textwrap.wrap(full_text, 16, break_long_words=True)
+    # lines = textwrap.wrap(full_text, 16, break_long_words=True)
+    lines = full_text.TextWrapper(width=16, break_long_words=False, replace_whitespace=False)
     display.lcd_display_string(lines[0], 1)  # Write line of text to first line of display
     if len(lines) > 1:
         display.lcd_display_string(lines[1], 2)  # Write line of text to second line of display
