6a2bf5f4a98e856724ffca4d1db5a1e7d1746275
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2021-12-26 03:20:54 +0800

    wait for script before reboot

--- reboot.py
+++ reboot.py
@@ -2,8 +2,7 @@ import lcd_display
 import os_manager
 
 
-if os_manager.isRPI:
-    lcd_display.show_on_lcd_line("REBOOTING", 1)
-    lcd_display.show_on_lcd_line("Approx 1 min", 2)
+lcd_display.show_on_lcd_line("REBOOTING", 1)
+lcd_display.show_on_lcd_line("Approx 1 min", 2)
 
 print("Rebooting")
