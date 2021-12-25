4bb2b00e23700a294ae4a734041d6c3a8b19fc63
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2021-12-26 02:36:56 +0800

    add boot instructions

---
+++ boot_instructions.py
@@ -0,0 +1,6 @@
+print("HELLO!")
+print("enter 'sh wifi.sh' to set up wifi")
+print("enter 'sh init.sh' to set up UID. << You need to set up UID of your account."
+      " For device to work. You'll get more instructions after entering 'sh init.sh'")
+print("enter 'update.sh' to update pager software. It should only take 10 seconds. ")
+print("enter 'sudo reboot' to restart device")
