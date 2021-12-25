d59af167c45a200324b73b7c8afa86f97b80e3fa
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2021-12-26 02:17:29 +0800

    add request event

--- wifi.py
+++ wifi.py
@@ -2,6 +2,19 @@ import re
 import os
 
 
+def request_event():
+    op = input("Enter\n1 to add wifi. \n2 to reset all connections\nx to exit\n")
+    if op == "1":
+        request_add_network()
+    elif op == "2":
+        reset_wpa()
+    elif op == "x":
+        return 0
+    else:
+        request_event()
+    return 0
+
+
 def request_add_network():
     ssid = input("Enter SSID(wifi name, case sensitive): ")
     psk = input("Enter wifi password(case sensitive): ")
@@ -20,4 +33,10 @@ def add_wifi(ssid, psk):
         f.truncate()
 
 
-request_add_network()
+def reset_wpa():
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    path = os.path.join(__location__, 'wpa_supplicant.conf')
+
+
+
+request_event()
