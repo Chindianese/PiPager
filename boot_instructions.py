be5aa39f4393d73b8ddd755713ea702461166b92
Tay Hao Cheng <tayhaocheng.media@gmail.com>
2021-12-26 13:44:53 +0800

    remove .sh from instructions

--- boot_instructions.py
+++ boot_instructions.py
@@ -1,7 +1,7 @@
 print("HELLO!-----------------")
 print("Pager version: 0.1.0.2")
-print("enter 'sh wifi.sh' to set up wifi")
-print("enter 'sh init.sh' to set up UID. << You need to set up UID of your account "
-      "for the device to work. You'll get more instructions after entering 'sh init.sh'")
-print("enter 'update.sh' to update pager software. It should only take about 10 seconds. ")
-print("enter 'sh reboot.sh' to restart device")
+print("enter 'sh wifi' to set up wifi")
+print("enter 'sh init' to set up UID. << You need to set up UID of your account "
+      "for the device to work. You'll get more instructions after entering 'sh init'")
+print("enter 'update' to update pager software. It should only take about 10 seconds. ")
+print("enter 'sh reboot' to restart device")
